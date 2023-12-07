88e948d46e7265bab2ae2ec439756da6d5b962bc
Ali Mostafavi <aliimostafavi@outlook.com>
2023-12-07 12:19:45 +0330

    .

--- app/page.jsx
+++ app/page.jsx
@@ -6,20 +6,20 @@ const page = () => {
   return (
     <>
       <section className="flex h-screen items-center justify-center gap-14 max-md:h-full max-md:flex-col max-md:py-20">
-        <div className="rounded-full border-2 border-primary">
+        <div className="rounded-full border-2 border-primary p-1 max-sm:mb-[-1.5rem]">
           <Image
             src="/images/profile.jpg"
             width={450}
-            height={0}
-            className="rounded-full max-lg:w-[370px] max-sm:w-[200px]"
+            height={450}
+            className="rounded-full max-lg:w-[370px] max-sm:w-[220px]"
             alt=""
           />
         </div>
         <div className="flex flex-col max-md:items-center">
-          <h1 className="mb-8 text-7xl font-extrabold text-light max-lg:text-6xl max-sm:text-5xl">
+          <h1 className="mb-8 text-7xl font-extrabold text-light max-lg:text-6xl max-sm:text-[2.6rem]">
             علی مصطفوی
           </h1>
-          <p className="mb-10 text-2xl font-medium text-text">
+          <p className="mb-10 text-xl font-medium text-text">
             برنامه نویس و طراح وب
           </p>
           <div className="mb-10 flex gap-3">
