a4da502fa84176799851ff626c1d7174fd24fcc4
Ali Mostafavi <aliimostafavi@outlook.com>
2023-12-08 14:42:07 +0330

    .

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,3 +1,11 @@
-const page = () => {};
+const page = () => {
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="text-5xl font-extrabold text-light">به زودی...</h1>
+      </div>
+    </>
+  );
+};
 
 export default page;
